9eeac40736073b06b1a90bf0aa64cfba73cfd614
Babatunde-Segun <officiallyvincify@gmail.com>
2024-11-13 08:27:24 +0100

    using express and testing the model

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res
+    .status(400)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can send to this endpoint...');
+});
+
 const port = 4000;
 app.listen(port, () => {
   console.log(`App running on prot ${port}...`);

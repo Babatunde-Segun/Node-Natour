525d8c632c452223199a2b1406cbc21f630fbc4a
Babatunde-Segun <officiallyvincify@gmail.com>
2024-11-14 01:22:13 +0100

    handle post and patch  request

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(400)
@@ -19,11 +21,67 @@ const tours = JSON.parse(
 
 // console.log(tours);
 
-app.get('/api/v1/tours', (req, res) => {
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (id > tours.length || !tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
   res.status(200).json({
     status: 'success',
-    results: tours.length,
-    data: { tours },
+    data: { tour },
+  });
+});
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    success: 'success',
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+  // res.send('Done');
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params.id);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Not found',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Updated tours',
+    },
   });
 });
 

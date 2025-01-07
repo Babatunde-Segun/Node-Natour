c57afa20df3911f6c643b2fbbbb88a1982e18763
Babatunde-Segun <officiallyvincify@gmail.com>
2025-01-07 22:21:18 +0100

    create Tour model and schema

--- server.js
+++ server.js
@@ -12,6 +12,24 @@ mongoose.connect(DB).then((con) => {
   console.log('DB connection successful', con.connections);
 });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'A tour must have a name'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);

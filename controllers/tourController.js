8340604cb79520e6929836aaba5c63a1f8418564
Babatunde-Segun <officiallyvincify@gmail.com>
2025-01-10 00:00:00 +0100

    implement deleteTour function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,11 +6,19 @@ const Tour = require('./../models/tourModel');
 // );
 
 // ROUTE HANDLERS
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Something went wrong',
+    });
+  }
 };
 
 exports.getTour = async (req, res) => {
@@ -71,9 +79,21 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: 'Welcome',
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    console.log(tour);
+    res.status(200).json({
+      status: 'success',
+      data: tour,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Something went wrong.',
+    });
+  }
 };

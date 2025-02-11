6c9972aa220e05d81c4deca6c8eeb6bdf9ff9f1b
Babatunde-Segun <officiallyvincify@gmail.com>
2025-02-11 09:52:04 +0100

    create new user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const router = express.Router();
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+const router = express.Router();
 
 const { getAllUsers, getUser, createUser, deleteUser, updateUser } =
   userController;
+
+router.post('/signup', authController.signup);
 
 router.route('/:id').get(getUser).delete(deleteUser).patch(updateUser);
 

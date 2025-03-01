21de674f5a4b0bbeebfe560813265d9d34f9d33b
Babatunde-Segun <officiallyvincify@gmail.com>
2025-03-01 01:41:13 +0100

    implement update me section

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,7 +13,8 @@ router.post('/login', authController.login);
 router.patch('/resetPassword/:token', authController.resetPassword);
 router.post('/forgetPassword', authController.forgetPassword);
 
-router.patch('/updateMyPassword', authController.protect,authController.updatePassword)
+router.patch('/updateMyPassword', authController.protect, authController.updatePassword)
+router.patch('/updateMe', authController.protect, userController.updateMe)
 
 router.route('/:id').get(getUser).delete(deleteUser).patch(updateUser);
 

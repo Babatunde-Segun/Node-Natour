e6328751494e56d0fdd445dce85e218931989b59
Babatunde-Segun <officiallyvincify@gmail.com>
2025-03-03 23:51:57 +0100

    implement data sanitization

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
 
 // console.log('this is morgan', morgan);
 
@@ -32,6 +34,13 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({limit: '10kb'}));
+
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+// Data sanitization against XSS
+app.use(xss())
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));

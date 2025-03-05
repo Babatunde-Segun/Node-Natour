3366d5d187d38cf38ec42caff6ba06316f5f16b6
Babatunde-Segun <officiallyvincify@gmail.com>
2025-03-05 10:03:12 +0100

    model locations(Geospatial Data)

--- models/tourModel.js
+++ models/tourModel.js
@@ -80,6 +80,29 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        day: Number,
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },

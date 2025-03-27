391b86ddef3c24a175dde958f5801b18e8f859d7
Babatunde-Segun <officiallyvincify@gmail.com>
2025-03-27 08:21:05 +0100

    update review with post

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -45,7 +45,6 @@ reviewSchema.pre(/^find/, function (next) {
 });
 
 reviewSchema.statics.calcAverageRatings = async function (tourId) {
- 
   const stats = await this.aggregate([
     {
       $match: { tour: tourId },
@@ -61,19 +60,43 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
 
   console.log(stats);
 
- await Tour.findByIdAndUpdate(tourId, {
+  await Tour.findByIdAndUpdate(tourId, {
     ratingsQuantity: stats[0].nRating,
-    ratingsAverage: stats[0].avgRating
-  })
+    ratingsAverage: stats[0].avgRating,
+  });
 };
-
 
 reviewSchema.post('save', function () {
   // this point to current review
-  this.constructor.calcAverageRatings(this.tour)
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+// findByIdandUpdate
+// findByIdandDelete
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  const r = await this.model.findOne();
 
 
- 
+
+  console.log(r);
+
 })
+
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.model.findOne();
+
+
+
+  console.log(this.r);
+  next()
+})
+
+reviewSchema.post(/^findOneAnd/, async function (next) {
+//  await this.model.findOne() doesn't work query has already executed
+  await this.r.constructor.calcAverageRatings(this.r.tour)
+})
+
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;

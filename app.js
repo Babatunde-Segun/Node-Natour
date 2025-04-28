f99982f724804944e913c80a9f34c1c98fed89a7
Babatunde-Segun <officiallyvincify@gmail.com>
2025-04-28 22:51:51 +0100

    setting up pug

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const rateLimit = require('express-rate-limit');
-const helmet = require('helmet')
-const mongoSanitize = require('express-mongo-sanitize')
-const xss = require('xss-clean')
-const hpp = require('hpp')
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 // console.log('this is morgan', morgan);
 
 const tourRoutes = require('./../4-NATOURS/routes/tourRoutes');
@@ -17,10 +18,16 @@ const reviewRoutes = require('./../4-NATOURS/routes/reviewRoutes');
 // middleware
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.set(path.join(__dirname, 'public'));
+
 // 1) GLOBAL MIDDLEWARES
 // Set Security HTTP headers
-app.use(helmet())
-
+app.use(helmet());
 
 // Development logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
@@ -34,23 +41,26 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json({limit: '10kb'}));
-
+app.use(express.json({ limit: '10kb' }));
 
 // Data sanitization against NoSQL query injection
-app.use(mongoSanitize())
+app.use(mongoSanitize());
 
 // Data sanitization against XSS
-app.use(xss())
+app.use(xss());
 
 // Prevent parameter pollution
-app.use(hpp({
-  whitelist: ['duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'price']
-}))
-
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'price',
+    ],
+  }),
+);
 
 app.use(morgan('dev'));
 
@@ -62,9 +72,12 @@ app.use((req, res, next) => {
 });
 
 // ROUTE
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/reviews', reviewRoutes)
+app.use('/api/v1/reviews', reviewRoutes);
 
 // Handle error for undefined route
 app.all('*', (req, res, next) => {
